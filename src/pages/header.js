98475ad7312e30a5d17b5f2b6906d75c5c9dbdd9
JANE LY <chimkanitha@gmail.com>
2018-10-29 14:33:36 +0700

    add logo

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,7 +1,27 @@
 import React from "react"
+import logo from '../images/logo.png'
+import { Link } from 'gatsby'
+
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem`, marginTop: `1.5rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
 export default () => (
-  <div>
-    <h1>Header</h1>
-  </div>
+  <nav class="navbar navbar-light bg-light">
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <a class="navbar-brand" href="#">
+          <img src={logo} alt="Logo" />
+        </a>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>
+  </nav>
+
 )
